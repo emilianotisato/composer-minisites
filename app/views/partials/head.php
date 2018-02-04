453b337fdc01844a629a942fe4932ce84c006b7e
Emiliano Tisato <emilianotisato@gmail.com>
2018-02-04 17:18:28 -0300

    Move config and add htacces ingnore all files

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="<?php \App\Core\App::get('config')['lang'] ?>">
+<html lang="<?php echo \App\Core\App::get('config')['lang'] ?>">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?php \App\Core\App::get('config')['site'] ?></title>
+        <title><?php echo \App\Core\App::get('config')['site']; ?></title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,7 +3,7 @@
 use App\Core\App;
 use App\Core\Database\{QueryBuilder, Connection};
 
-App::bind('config', require 'config.php');
+App::bind('config', require 'app/config.php');
 
 // Uncomment if you'll se a database
 // App::bind('database', new QueryBuilder(
